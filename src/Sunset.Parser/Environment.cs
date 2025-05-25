1faa3365a938f68ac494f33df555a15a9a483cbe
Nick Boey <nboey@northrop.com.au>
2025-05-26 08:17:36 +1000

    Add environment and scope classes

---
+++ src/Sunset.Parser/Environment.cs
@@ -0,0 +1,17 @@
+﻿namespace Sunset.Parser;
+
+/// <summary>
+/// The execution environment for the interpreter, containing all the variables and their values from the source code.
+/// </summary>
+public class Environment
+{
+    public List<Scope> Scopes { get; } = [];
+
+    public void AddFile(string fileName)
+    {
+    }
+
+    public void AddSource(string source)
+    {
+    }
+}
---
+++ src/Sunset.Parser/Scope.cs
@@ -0,0 +1,14 @@
+﻿using Sunset.Parser.Variables;
+
+namespace Sunset.Parser;
+
+/// <summary>
+/// Contains a scope of variables and their values.
+/// </summary>
+public class Scope
+{
+    public string Name { get; }
+    public string ScopePath { get; }
+    public List<Variable> Variables { get; }
+    public List<Scope> ChildScopes { get; } = [];
+}

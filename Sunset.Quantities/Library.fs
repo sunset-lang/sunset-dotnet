6f75580ba30f8becd1f1a4cdf779497e534fd1ba
Nick Boey <nboey@northrop.com.au>
2025-06-05 09:30:55 +1000

    Start rewrite of units library in F#

---
+++ Sunset.Quantities/Library.fs
@@ -0,0 +1,9 @@
+﻿namespace Sunset.Quantities
+
+/// <summary>
+/// Represents a dimension
+/// </summary>
+type Dimension = { Power: int; Factor: double }
+
+module Say =
+    let hello name = printfn "Hello %s" name
